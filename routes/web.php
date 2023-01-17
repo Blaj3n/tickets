3fcb9ec035ff8035e43ca6e417314a63ec261952
Blaj3n <benceulrich@gmail.com>
2023-01-17 11:25:57 +0100

    web.php Controllerek View-k hozzáadása

--- routes/web.php
+++ routes/web.php
@@ -26,18 +26,28 @@ Route::get('/api/tickets/{id}', [TicketController::class, 'show']);
 Route::delete('/api/tickets/{id}', [TicketController::class, 'destroy']);
 Route::post('/api/tickets', [TicketController::class, 'store']);
 Route::put('/api/tickets/{id}', [TicketController::class, 'update']);
+Route::get("/ticket/new", [TicketController::class, "newView"]);
+Route::get("/ticket/edit/{id}", [TicketController::class, "editView"]);
+Route::get("/ticket/list", [TicketController::class, "lisView"]);
+
 //EVENT
 Route::get("/api/events", [EventController::class, "index"]);
 Route::get('/api/events/{id}', [EventController::class, 'show']);
 Route::delete('/api/events/{id}', [EventController::class, 'destroy']);
 Route::post('/api/events', [EventController::class, 'store']);
 Route::put('/api/events/{id}', [EventController::class, 'update']);
+Route::get("/event/new", [TicketController::class, "newView"]);
+Route::get("/event/edit/{id}", [TicketController::class, "editView"]);
+Route::get("/event/list", [TicketController::class, "lisView"]);
 //TRADER
 Route::get("/api/traders", [TraderController::class, "index"]);
 Route::get('/api/traders/{id}', [TraderController::class, 'show']);
 Route::delete('/api/traders/{id}', [TraderController::class, 'destroy']);
 Route::post('/api/traders', [TraderController::class, 'store']);
 Route::put('/api/traders/{id}', [TraderController::class, 'update']);
+Route::get("/trader/new", [TicketController::class, "newView"]);
+Route::get("/trader/edit/{id}", [TicketController::class, "editView"]);
+Route::get("/trader/list", [TicketController::class, "lisView"]);
 //traderBetu
 Route::get("/api/traderBetu{betu}", [TraderController::class, "traderBetu"]);
 //traderDb
